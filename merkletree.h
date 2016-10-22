a81c22fc7df1093f7e0e9e3e75fe2104c6b52ba1
aanrv <varan9595@yahoo.com>
2016-10-22 17:44:59 -0400

    update merkletree header

--- merkletree.h
+++ merkletree.h
@@ -6,33 +6,43 @@
 #include <array>
 #include <cryptopp/sha.h>
 
-typedef CryptoPP::SHA256 HashFunction;
-typedef std::string ItemType; 
-static const size_t HASHSIZE = CryptoPP::SHA256::DIGESTSIZE;
-
 class MerkleNode;
 
 class MerkleTree {
 public:
+	typedef CryptoPP::SHA256 HashFunction;				/* SHA256 will be used as the hash function */
+	typedef std::string ItemType;					/* The tree will hold hashes of strings */
+	static const size_t HASHSIZE = CryptoPP::SHA256::DIGESTSIZE;	/* Size of the hashes */
+	typedef std::array<byte, MerkleTree::HASHSIZE> HashArray;	/* std::array will be used to store hashes */
+
 	MerkleTree(std::vector<ItemType> items);
 	~MerkleTree();
 
 	/* Retrieve tree's merkle root */
-	std::array<byte, HASHSIZE> getMerkleRoot() const;
+	HashArray getMerkleRoot() const;
 
 	/* Retrieve merkle path of element. Returns empty vector if element does not exist. */
-	std::vector<std::array<byte, HASHSIZE>> getMerklePath(ItemType item) const;
+	std::vector<HashArray> getMerklePath(ItemType item) const;
 private:
+	/* Given a list of items and a position within a tree, calculates the hash and creates a node. */
+	MerkleNode* calculateNode(int distanceFromLeaves, std::vector<MerkleTree::ItemType>& items);
+
+	/* Given two hashes, concats them and calculates their hash */
+	HashArray concatHash(HashArray a, HashArray b) const;
+
 	MerkleNode* head;
 };
 
 class MerkleNode {
 public:
-	MerkleNode(std::array<byte, HASHSIZE> hash);
+	MerkleNode(MerkleTree::HashArray hash);
 	~MerkleNode();
 
 	MerkleNode* left;
 	MerkleNode* right;
+
+private:
+	MerkleTree::HashArray hash;
 };
 
 #endif	// MERKLETREE_H
