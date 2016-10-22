7d36fc2518cdd3d5224e3e34a3f01f58c56b317a
aanrv <varan9595@yahoo.com>
2016-10-22 18:18:06 -0400

    created merklenode header

---
+++ merklenode.h
@@ -0,0 +1,23 @@
+#ifndef MERKLENODE_H
+#define MERKLENODE_H
+
+#include <cryptopp/sha.h>
+#include <array>
+
+class MerkleNode {
+public:
+	typedef CryptoPP::SHA256 HashFunction;				/* SHA256 will be used as the hash function */
+	static const size_t HASHSIZE = CryptoPP::SHA256::DIGESTSIZE;	/* Size of the hashes */
+	typedef std::array<byte, HASHSIZE> HashArray;			/* std::array will be used to store hashes */
+
+	MerkleNode(HashArray hash);
+	~MerkleNode();
+
+	MerkleNode* left;
+	MerkleNode* right;
+private:
+	HashArray hash;
+};
+
+#endif	// MERKLENODE_H
+
--- merkletree.h
+++ merkletree.h
@@ -3,46 +3,28 @@
 
 #include <vector>
 #include <string>
-#include <array>
-#include <cryptopp/sha.h>
-
-class MerkleNode;
+#include "merklenode.h"
 
 class MerkleTree {
 public:
-	typedef CryptoPP::SHA256 HashFunction;				/* SHA256 will be used as the hash function */
-	typedef std::string ItemType;					/* The tree will hold hashes of strings */
-	static const size_t HASHSIZE = CryptoPP::SHA256::DIGESTSIZE;	/* Size of the hashes */
-	typedef std::array<byte, MerkleTree::HASHSIZE> HashArray;	/* std::array will be used to store hashes */
+	typedef std::string ItemType;
 
 	MerkleTree(std::vector<ItemType> items);
 	~MerkleTree();
 
 	/* Retrieve tree's merkle root */
-	HashArray getMerkleRoot() const;
+	MerkleNode::HashArray getMerkleRoot() const;
 
 	/* Retrieve merkle path of element. Returns empty vector if element does not exist. */
-	std::vector<HashArray> getMerklePath(ItemType item) const;
+	std::vector<MerkleNode::HashArray> getMerklePath(ItemType item) const;
 private:
 	/* Given a list of items and a position within a tree, calculates the hash and creates a node. */
 	MerkleNode* calculateNode(int distanceFromLeaves, std::vector<MerkleTree::ItemType>& items);
 
 	/* Given two hashes, concats them and calculates their hash */
-	HashArray concatHash(HashArray a, HashArray b) const;
+	MerkleNode::HashArray concatHash(MerkleNode::HashArray a, MerkleNode::HashArray b) const;
 
 	MerkleNode* head;
-};
-
-class MerkleNode {
-public:
-	MerkleNode(MerkleTree::HashArray hash);
-	~MerkleNode();
-
-	MerkleNode* left;
-	MerkleNode* right;
-
-private:
-	MerkleTree::HashArray hash;
 };
 
 #endif	// MERKLETREE_H
